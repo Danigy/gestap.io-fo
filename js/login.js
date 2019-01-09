72560f5330c9fb16f5e0a7a10e4aacdee7457e0e
DavidCaraiannis <david.caraiannis@gmail.com>
2019-01-09 10:30:34 +0100

    modif login et ajout inscription

--- js/login.js
+++ js/login.js
@@ -11,21 +11,38 @@ $(document).ready(function(){
 
 	// Appel Ajax pour se connecter
 	$("#loginForm").submit(function(event){
-	  event.preventDefault(); //prevent default action
-		console.log('three');
-	   // var post_url = $(this).attr("action"); //get form action url
-	   // var request_method = $(this).attr("method"); //get form GET/POST method
-		 var json = {};
-		 json['login'] = $('login_username').value();
-		 json['password'] = $('login_password').value();
-		 console.log('json', json);
-	   $.ajax({
-	       url : 'dashboard.php',
-	       type: 'GET',
-	       data : json,
-	   }).done(function(response){ //
-	       window.location.href = "http://google.com";
-	   });
+		event.preventDefault(); //prevent default action
+		var json = {};
+		json['login'] = $('#login_username')[0].value;
+		json['password'] = $('#login_password')[0].value;
+	  $.ajax({
+	      url : 'http://192.168.33.10/gestap.io-fo/test.txt',
+	      type: 'GET',
+	      data : json,
+	  }).done(function(result){
+				if(result == true)
+	      	window.location.href = "http://google.com";
+				else
+					alert('wrong username or password');
+	  });
+	});
+
+	$("#signupForm").submit(function(event){
+		event.preventDefault(); //prevent default action
+		var json = {};
+		json['login'] = $('#signup_username')[0].value;
+		json['email'] = $('#signup_email')[0].value;
+		json['password'] = $('#signup_password')[0].value;
+	  $.ajax({
+	      url : 'http://192.168.33.10/gestap.io-fo/test.txt',
+	      type: 'GET',
+	      data : json,
+	  }).done(function(result){
+				if(result == true)
+	      	window.location.href = "http://google.com";
+				else
+					alert('An error occured. Please try again or contact administrator.');
+	  });
 	});
 
 })
