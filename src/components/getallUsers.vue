62c0081cfceae3027b6feb2898bea2cfe990bfda
morganplouchart <morgan.plouchart@yahoo.fr>
2019-01-10 15:08:42 +0100

    Intégration vue

---
+++ src/components/getallUsers.vue
@@ -0,0 +1,30 @@
+<template>
+</template>
+<script>
+export default {
+  name: 'getallUsers',
+  components: {
+    getallUsers
+  },
+  data: function() {
+    return {
+      allUsers: ''
+    }
+  },
+  methods: {
+    getAllUsers: function() {
+      axios.get('http://ebcitakademy.alwaysdata.net/users/all')
+        .then((response) => {
+          this.allUsers = response.data.list_of_users
+          console.log(response)
+        })
+        .catch((response) => {
+          Alert("Erreur de chargement de l'API")
+        })
+    }
+  },
+  mounted() {
+    this.getAllUsers()
+  }
+}
+</script>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,20 @@
+
+<template>
+<h1>Dashboard</h1>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Dashboard',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
