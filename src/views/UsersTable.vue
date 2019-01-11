690c57c7905252d725f028875f6313dbd5ff9256
DavidCaraiannis <david.caraiannis@gmail.com>
2019-01-11 16:19:23 +0100

    avancement users table

--- src/views/UsersTable.vue
+++ src/views/UsersTable.vue
@@ -1,231 +1,64 @@
 <template>
-<b-container fluid>
-  <!-- User Interface controls -->
-  <b-row>
-    <b-col md="6" class="my-1">
-      <b-form-group horizontal label="Filter" class="mb-0">
-        <b-input-group>
-          <b-form-input v-model="filter" placeholder="Type to Search" />
-          <b-input-group-append>
-            <b-btn :disabled="!filter" @click="filter = ''">Clear</b-btn>
-          </b-input-group-append>
-        </b-input-group>
-      </b-form-group>
-    </b-col>
-    <b-col md="4" class="my-1">
-      <b-form-group horizontal label="Per page" class="mb-0">
-        <b-form-select :options="pageOptions" v-model="perPage" />
-      </b-form-group>
-    </b-col>
-  </b-row>
-
-  <!-- Main table element -->
-  <b-table show-empty stacked="md" :items="items" :fields="fields" :current-page="currentPage" :per-page="perPage" :filter="filter" :sort-by.sync="sortBy" :sort-desc.sync="sortDesc" :sort-direction="sortDirection" @filtered="onFiltered">
-    <template slot="name" slot-scope="row">{{row.value.first}} {{row.value.last}}</template>
-    <template slot="isActive" slot-scope="row">{{row.value?'Yes :)':'No :('}}</template>
-    <template slot="actions" slot-scope="row">
-      <!-- We use @click.stop here to prevent a 'row-clicked' event from also happening -->
-      <b-button size="sm" @click.stop="info(row.item, row.index, $event.target)" class="mr-1">
-        Info modal
-      </b-button>
-      <b-button size="sm" @click.stop="row.toggleDetails">
-        {{ row.detailsShowing ? 'Hide' : 'Show' }} Details
-      </b-button>
-    </template>
-    <template slot="row-details" slot-scope="row">
-      <b-card>
-        <ul>
-          <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value}}</li>
-        </ul>
-      </b-card>
-    </template>
-  </b-table>
-
-  <b-row>
-    <b-col md="6" class="my-1">
-      <b-pagination :total-rows="totalRows" :per-page="perPage" v-model="currentPage" class="my-0" />
-    </b-col>
-  </b-row>
-
-  <!-- Info modal -->
-  <b-modal id="modalInfo" @hide="resetModal" :title="modalInfo.title" ok-only>
-    <pre>{{ modalInfo.content }}</pre>
-  </b-modal>
-
-</b-container>
+<div>
+  <h1>Users</h1>
+  <table class="table table-bordered table-hover">
+    <thead>
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Birthday</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="user in users">
+        <th>{{ user.firstname }}</th>
+        <td>{{ user.lastname }}</td>
+        <td>{{ user.email }}</td>
+        <td>{{ user.birthday }}</td>
+        <td>
+          <button class="btn btn-primary" @click="updateUser(user)">Modify</button>
+          <button class="btn btn-danger" @click="deleteUser(user)"> X</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 </template>
 
 <script>
-const items = [{
-    isActive: true,
-    age: 40,
-    name: {
-      first: 'Dickerson',
-      last: 'Macdonald'
-    }
-  },
-  {
-    isActive: false,
-    age: 21,
-    name: {
-      first: 'Larsen',
-      last: 'Shaw'
-    }
-  },
-  {
-    isActive: false,
-    age: 9,
-    name: {
-      first: 'Mini',
-      last: 'Navarro'
-    },
-    _rowVariant: 'success'
-  },
-  {
-    isActive: false,
-    age: 89,
-    name: {
-      first: 'Geneva',
-      last: 'Wilson'
-    }
-  },
-  {
-    isActive: true,
-    age: 38,
-    name: {
-      first: 'Jami',
-      last: 'Carney'
-    }
-  },
-  {
-    isActive: false,
-    age: 27,
-    name: {
-      first: 'Essie',
-      last: 'Dunlap'
-    }
-  },
-  {
-    isActive: true,
-    age: 40,
-    name: {
-      first: 'Thor',
-      last: 'Macdonald'
-    }
-  },
-  {
-    isActive: true,
-    age: 87,
-    name: {
-      first: 'Larsen',
-      last: 'Shaw'
-    },
-    _cellVariants: {
-      age: 'danger',
-      isActive: 'warning'
-    }
-  },
-  {
-    isActive: false,
-    age: 26,
-    name: {
-      first: 'Mitzi',
-      last: 'Navarro'
-    }
-  },
-  {
-    isActive: false,
-    age: 22,
-    name: {
-      first: 'Genevieve',
-      last: 'Wilson'
-    }
-  },
-  {
-    isActive: true,
-    age: 38,
-    name: {
-      first: 'John',
-      last: 'Carney'
-    }
-  },
-  {
-    isActive: false,
-    age: 29,
-    name: {
-      first: 'Dick',
-      last: 'Dunlap'
-    }
-  }
-]
+import axios from 'axios'
 
 export default {
+  name: 'ListUsers',
+  components: {},
   data() {
     return {
-      items: items,
-      fields: [{
-          key: 'name',
-          label: 'Person Full name',
-          sortable: true,
-          sortDirection: 'desc'
-        },
-        {
-          key: 'age',
-          label: 'Person age',
-          sortable: true,
-          'class': 'text-center'
-        },
-        {
-          key: 'isActive',
-          label: 'is Active'
-        },
-        {
-          key: 'actions',
-          label: 'Actions'
-        }
-      ],
-      currentPage: 1,
-      perPage: 5,
-      totalRows: items.length,
-      pageOptions: [5, 10, 15],
-      sortBy: null,
-      sortDesc: false,
-      sortDirection: 'asc',
-      filter: null,
-      modalInfo: {
-        title: '',
-        content: ''
-      }
-    }
-  },
-  computed: {
-    sortOptions() {
-      // Create an options list from our fields
-      return this.fields
-        .filter(f => f.sortable)
-        .map(f => {
-          return {
-            text: f.label,
-            value: f.key
-          }
-        })
-    }
+      users: []
+    };
   },
   methods: {
-    info(item, index, button) {
-      this.modalInfo.title = `Row index: ${index}`
-      this.modalInfo.content = JSON.stringify(item, null, 2)
-      this.$root.$emit('bv::show::modal', 'modalInfo', button)
+    getUsers: function() {
+      axios.get('http://ebcitakademy.alwaysdata.net/users/all')
+        .then((response) => {
+          this.users = response.data.list_of_users
+        })
+        .catch((response) => {
+          alert('An error occured. Please try again or contact administrator.')
+        })
     },
-    resetModal() {
-      this.modalInfo.title = ''
-      this.modalInfo.content = ''
+    deleteUser: function(user) {
+      const url = `http://ebcitakademy.alwaysdata.net/users/delete?id=${user._id}`
+      axios.delete(url)
+      return location.reload()
     },
-    onFiltered(filteredItems) {
-      // Trigger pagination to update the number of buttons/pages due to filtering
-      this.totalRows = filteredItems.length
-      this.currentPage = 1
+    updateUser: function(user) {
+      $('#modifyUser').remove('hidden')
     }
-  }
+  },
+  mounted() {
+    this.getUsers()
+  },
 }
 </script>
